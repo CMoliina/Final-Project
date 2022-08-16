a6b0574ec6bf1698246029c84f14da99b36b1aa6
Camila Molina <molina.neculnir@gmail.com>
2022-08-16 18:42:14 -0400

    list of every items

--- cryptoAppProject/App.js
+++ cryptoAppProject/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { FlatList, StyleSheet, View } from 'react-native';
 import CoinItem from './src/components/CoinItem';
-
+import cryptocurrency from './assets/data/cryptocurrencies.json';
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList/>
+      <FlatList
+        data={cryptocurrency}
+        renderItem={({item}) => <CoinItem marketCoin={item} />}
+      />
       <StatusBar style="light" />
     </View>
   );
