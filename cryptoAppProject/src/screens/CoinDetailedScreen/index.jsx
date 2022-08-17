f88bf5dadc65b9276678630a63015b3d35eef58c
Camila Molina <molina.neculnir@gmail.com>
2022-08-16 20:54:52 -0400

    some icons

--- cryptoAppProject/src/screens/CoinDetailedScreen/index.jsx
+++ cryptoAppProject/src/screens/CoinDetailedScreen/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { Ionicons, EvilIcons } from '@expo/vector-icons';
+import Coin from '../../../assets/data/crypto.json';
+
 const CoinDetailedScreen = () => {
+    const {
+        image: { small },
+        symbol,
+        name,
+        market_data: { market_cap_rank }
+    } = Coin;
     return (
-        <View>
-            <Text style={{color:'white'}}>Detailed Screen</Text>
+        <View style={{flexDirection:'row', paddingHorizontal:10 }}>
+            <Ionicons name="chevron-back-sharp" size={30} color="white" />
+            <Image source={{ uri: small }} style={{ width: 25, height: 25 }} />
+            <Text style={{ color: 'white' }}>{symbol.toUpperCase()}</Text>
+            <Text style={{ color: 'white' }}>#{market_cap_rank}</Text>
+            <EvilIcons name="user" size={30} color="white" />
         </View>
     );
 };
