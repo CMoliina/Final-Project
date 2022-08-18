03bf196a19ac5e8bbb8a7b31c251ce83ab1c4fab
Camila Molina <molina.neculnir@gmail.com>
2022-08-18 02:10:30 -0400

    reanimated chart works

--- cryptoAppProject/src/screens/CoinDetailedScreen/index.jsx
+++ cryptoAppProject/src/screens/CoinDetailedScreen/index.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 import Coin from '../../../assets/data/crypto.json';
 import CoinDetailedHeader from "./components/CoinDetailedHeader";
 import styles from "./styles";
 import { AntDesign } from '@expo/vector-icons';
+import { ChartDot, ChartPath, ChartPathProvider } from '@rainbow-me/animated-charts';
+
+
+
 
 const CoinDetailedScreen = () => {
     const {
         image: { small },
         symbol,
         name,
+        prices,
         market_data: {
             market_cap_rank,
             current_price,
@@ -18,34 +23,52 @@ const CoinDetailedScreen = () => {
 
     } = Coin;
 
-    const percentageColor = price_change_percentage_24h < 0 ? '#ea3943' : '#16c784'
+    const percentageColor = price_change_percentage_24h < 0 ? '#ea3943' : '#16c784';
 
+    const ScreenWidth = Dimensions.get('window').width;
 
     return (
-        <View style={{ padding: 10 }}>
-            <CoinDetailedHeader
-                image={small}
-                symbol={symbol}
-                marketCapRank={market_cap_rank}
-            />
-            <View style={styles.priceContainer}>
+        <View style={{ paddingHorizontal: 10 }}>
+            <ChartPathProvider
+                data={{
+                    points: prices.map(([x, y]) => ({ x, y })),
+                    smoothingStrategy: 'bezier'
+                }}>
+                <CoinDetailedHeader
+                    image={small}
+                    symbol={symbol}
+                    marketCapRank={market_cap_rank}
+                />
+                <View style={styles.priceContainer}>
+                    <View>
+                        <Text style={styles.name}>{name}</Text>
+                        <Text style={styles.currentPrice}>${current_price.usd}</Text>
+                    </View>
+                    <View style={{ backgroundColor: percentageColor, paddingHorizontal: 3, paddingVertical: 8, borderRadius: 5, flexDirection: 'row' }}>
+                        <AntDesign
+                            name={price_change_percentage_24h < 0 ? 'caretdown' : 'caretup'}
+                            size={12}
+                            color={'white'}
+                            style={{ alignSelf: 'center', marginRight: 5 }}
+                        />
+                        <Text style={styles.priceChange}>
+                            {price_change_percentage_24h.toFixed(2)}%
+                        </Text>
+                    </View>
+                </View>
                 <View>
-                    <Text style={styles.name}>{name}</Text>
-                    <Text style={styles.currentPrice}>${current_price.usd}</Text>
+                    <ChartPath
+                        height={ScreenWidth / 2}
+                        stroke="yellow"
+                        width={ScreenWidth} />
+                    <ChartDot style={{ backgroundColor: 'white' }} />
                 </View>
-                <View style={{ backgroundColor: percentageColor, paddingHorizontal: 3, paddingVertical: 8, borderRadius: 5, flexDirection: 'row' }}>
-                    <AntDesign
-                        name={price_change_percentage_24h < 0 ? 'caretdown' : 'caretup'}
-                        size={12} 
-                        color={'white'}
-                        style={{ alignSelf: 'center', marginRight: 5 }}
-                    />
-                    <Text style={styles.priceChange}>
-                        {price_change_percentage_24h.toFixed(2)}%
-                    </Text>
-                </View>
-            </View>
+            </ChartPathProvider>
         </View>
+
+
+
+
     );
 };
 
--- cryptoAppProject/src/screens/CoinDetailedScreen/styles.js
+++ cryptoAppProject/src/screens/CoinDetailedScreen/styles.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
         color: 'white', 
         fontSize: 17, 
         fontWeight: '500'
-    }
+    },
 
 });
 
