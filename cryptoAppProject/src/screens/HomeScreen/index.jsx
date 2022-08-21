d560e08977e7f5fa0015d757e978236ef5cc3079
Camila Molina <molina.neculnir@gmail.com>
2022-08-20 21:04:15 -0400

    implementing API in homescreen

--- cryptoAppProject/src/screens/HomeScreen/index.jsx
+++ cryptoAppProject/src/screens/HomeScreen/index.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CoinItem from '../../components/CoinItem';
-import cryptocurrency from '../../../assets/data/cryptocurrencies.json';
-import { FlatList } from 'react-native';
+import { FlatList, RefreshControl } from 'react-native';
+import { getMarketData } from "../../services/requests";
 
 const HomeScreen = () => {
+    const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const fetchCoins = async () => {
+        setLoading(true);
+        const coinsData = await getMarketData()
+        setCoins(coinsData)
+        setLoading(false);
+    }
+
+    const refetchCoins = async () => {
+        setLoading(true);
+        const coinsData = await getMarketData()
+        setCoins(coinsData)
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchCoins()
+    }, [])
+
     return (
         <FlatList
-            data={cryptocurrency}
+            data={coins}
             renderItem={({ item }) => <CoinItem marketCoin={item} />}
+            refreshControl={
+                <RefreshControl
+                    refreshing={loading}
+                    onRefresh={refetchCoins}
+                />
+            }
         />
     );
 };
