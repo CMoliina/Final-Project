f5ee99fb3f78596b810e80a0c9ac9d94980d7154
Camila Molina <molina.neculnir@gmail.com>
2022-08-22 01:03:05 -0400

    refresh option works

--- cryptoAppProject/src/screens/HomeScreen/index.jsx
+++ cryptoAppProject/src/screens/HomeScreen/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
 import CoinItem from "../../components/CoinItem";
 import { getMarketData } from "../../services/requests";
 const HomeScreen = () => {
@@ -7,6 +7,13 @@ const HomeScreen = () => {
   const [loading, setLoading] = useState(false);
 
   const fetchCoins = async () => {
+    setLoading(true);
+    const coinsData = await getMarketData();
+    setCoins(coinsData);
+    setLoading(false);
+  }
+
+  const refetchCoins = async () => {
     setLoading(true);
     const coinsData = await getMarketData();
     setCoins(coinsData);
@@ -21,6 +28,13 @@ const HomeScreen = () => {
     <FlatList
       data={coins}
       renderItem={({ item }) => <CoinItem marketCoin={item} />}
+      refreshControl={
+        <RefreshControl
+          refreshing={loading}
+          tintColor="white"
+          onRefresh={refetchCoins}
+        />
+      }
     />
   );
 };
